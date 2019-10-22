17855142d628ec3decc81d916ab6632f21bbe272
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2019-10-22 21:38:44 +0530

    Added Code for Stream Create

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../redux/actions';
 
 export class StreamCreate extends Component {
   
@@ -25,8 +27,8 @@ export class StreamCreate extends Component {
     )
   }
 
-  onSubmit(formValues){
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
   
   render() {
@@ -54,7 +56,9 @@ const validate = formValues =>{
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
