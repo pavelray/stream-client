2ad4c7b1f3e1bac91c97badfd0a06a637f0f8e69
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2019-10-28 11:13:44 +0530

    Updated

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -39,7 +39,7 @@ export class StreamList extends Component {
           {this.renderActionButton(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
           <div className="description">{stream.description}</div>
           </div>
         </div>
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect } from 'react-redux';
+import {fetchStream} from '../../redux/actions';
 
 export class StreamShow extends Component {
+  componentDidMount(){
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
   render() {
+     if(!this.props.stream){
+       return <div>Loading..</div>
+     }
+    
+    const{title,description} = this.props.stream;
     return (
       <div>
-        StreamShow
+        <h1>{title}</h1>
+        <h5>description</h5>
       </div>
     )
   }
 }
 
-export default StreamShow
+const mapStateToProps=(state,ownProps)=>{
+  return {stream:state.streams[ownProps.match.params.id]}
+}
+
+export default connect(mapStateToProps,{fetchStream})(StreamShow)
