d3b124d05d5fa0599a6cdde63221f65a4b19dae1
Pavel Ray <35458270+pavelray@users.noreply.github.com>
2019-10-28 11:14:26 +0530

    Updated

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -16,7 +16,7 @@ export class StreamShow extends Component {
     return (
       <div>
         <h1>{title}</h1>
-        <h5>description</h5>
+        <h5>{description}</h5>
       </div>
     )
   }
